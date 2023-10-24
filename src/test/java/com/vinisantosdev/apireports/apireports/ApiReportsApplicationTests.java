13b2433c3e5fe7d0e7bd7afc532c4361fafa3b96
Vinicius Santos <viniciusilvasantos.contact@gmail.com>
2023-10-24 19:06:46 -0300

    only java

---
+++ src/test/java/com/vinisantosdev/apireports/apireports/ApiReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisantosdev.apireports.apireports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
